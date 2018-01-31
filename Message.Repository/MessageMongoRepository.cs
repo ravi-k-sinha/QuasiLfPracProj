419fc31b202ebb9fd4b0a4eb228b6f2bba92c9ca
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-31 12:40:52 +0530

    Skeleton implementation of IMessageRepository

---
+++ Message.Repository/MessageMongoRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Message.Repository
+{
+    public class MessageMongoRepository : IMessageRepository
+    {
+        public Task<bool> Add(MessageDetail item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IMessageDetail> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IMessageDetail>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> MarkSent(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(string id, MessageDetail updatedMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
