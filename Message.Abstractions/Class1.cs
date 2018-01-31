97b07a4ea598c8cabca6549c1a8dc9e51fc20d17
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-31 12:11:06 +0530

    Created interface abstraction of a Message

--- Message.Abstractions/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Message.Abstractions
-{
-    public class Class1
-    {
-    }
-}
---
+++ Message.Abstractions/IMessageDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Message
+{
+    public interface IMessageDetail
+    {
+        string MessageId { get; set; }
+
+        string tenantId { get; set; }
+
+        string Subject { get; set; }
+        string Body { get; set; }
+        DateTime CreatedOn { get; set; }
+        DateTime LastModifiedOn { get; set; }
+        DateTime SentOn { get; set; }
+        string Status { get; set; }
+        string SentBy { get; set; }
+        List<string> SentTo { get; set; }
+    }
+}
