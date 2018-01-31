824ff5a3ff1da67e8ca856b52de4b769d1b1edae
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-31 12:52:06 +0530

    Created Message Mongo context class

---
+++ Message.Repository/MessageMongoContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace Message.Repository
+{
+    class MessageMongoContext
+    {
+        public IMongoDatabase _db;
+        private string localMongoUrl = "mongodb://localhost:27017";
+
+        public MessageMongoContext()
+        {
+            var client = new MongoClient(localMongoUrl);
+            if (client != null)
+            {
+                _db = client.GetDatabase("Message1Db");
+            }
+        }
+
+        public IMongoCollection<MessageDetail> Items
+        {
+            get
+            {
+                return _db.GetCollection<MessageDetail>("messages");
+            }
+        }
+    }
+}
