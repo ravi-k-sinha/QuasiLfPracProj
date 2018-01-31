7b2379862f020fc2520134124169fa3033805e9f
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-31 12:33:32 +0530

    Created MessageDetail implementation and repository interface

---
+++ Message.Abstractions/MessageDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Message
+{
+    public class MessageDetail : IMessageDetail
+    {
+        [BsonId]
+        public string MessageId { get; set; }
+
+        public string tenantId { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public DateTime SentOn { get; set; }
+        public string Status { get; set; }
+        public string SentBy { get; set; }
+        public List<string> SentTo { get; set; }
+    }
+}
---
+++ Message.Abstractions/Repository/IMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message
+{
+    public interface IMessageRepository
+    {
+        Task<IEnumerable<IMessageDetail>> GetAll();
+        Task<IMessageDetail> Get(string id);
+        Task<bool> Add(MessageDetail item);
+        Task<bool> Delete(string id);
+        Task<bool> MarkSent(string id);
+        Task<bool> Update(string id, MessageDetail updatedMessage);
+    }
+}
